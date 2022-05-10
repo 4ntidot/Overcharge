065e9cf7237f41792aa461eabd8c98b4cbbcb3ef
Lockstart <98713994+Lockstart@users.noreply.github.com>
2022-05-10 22:25:18 +0200

    :zap: Edited cookies.js

--- src/js/cookies.js
+++ src/js/cookies.js
@@ -1,5 +1,5 @@
 let browsercookies = localStorage.getItem("overchargeBrowser");
-fetch("data/cookies.json") {
+fetch("data/cookies.json")
 	.then(res => res.json())
 	.then(data => {
 		if (browsercookies == undefined || browsercookies == null) {
@@ -7,3 +7,19 @@ fetch("data/cookies.json") {
 			localStorage.setItem("overchargeBrowser", browsercookies);
 		}
 	})
+
+function loadHistory() {
+	const historyContainer = document.getElementById("history");
+	const history = browsercookies.history;
+	var i = 0;
+	for (i==0, i < history.length, i++) {
+		const section = historyContainer.innerHTML;
+		const addition `
+		<li class="shortcut"><a href="${history[i].url}" target="webview">
+			<span class="shortcut-title">${history[i].title}</span><br>
+			<span class="shortcut-url">${history[i].url}</span>
+		</a></li>`
+		historyContainer.innerHTML = section + addition;
+		i += 1;
+	}
+}
