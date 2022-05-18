43aa237d59d6522230c438d7089ae0081ef7955e
Lockstart <98713994+Lockstart@users.noreply.github.com>
2022-05-18 19:11:18 +0200

    Create index.js

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+function initialize() {
+    const window = document.getElementById("webview");
+    const tabList = localStorage.getItem("overchargeBrowser").tabList;
+}
+
+function createTab(url) {
+   
+}
